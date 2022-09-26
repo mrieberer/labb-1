5d676d90aa3f24a8d3685350643116f5433776b3
Michael Rieberer <mrieberer@gmail.com>
2022-09-26 22:28:51 +0200

    kopierade i Program.cs i projektet

---
+++ konsolAppLabb-1/konsolAppLabb-1/Program.cs
@@ -0,0 +1,23 @@
+﻿void csHTMLgenerator(string KLASSNAMN, string[] meddelande)
+{
+    Console.WriteLine($"<!DOCTYPE html>\n<body>\n<h1>Välkomna {KLASSNAMN}</h1>");
+
+    customMessage(meddelande, 1);
+    //$"<p><b>Meddelande 1:</b> {welcomeMessage(meddelande1)}.</p>");
+    //Console.WriteLine($"<p><b>Meddelande 2:</b> {meddelande2}.</p>");
+    Console.WriteLine("</main>\n</body>\n</html>");
+}
+
+void customMessage(string[] inMeddelande, int messageNumber)
+{
+    //messageNumber = 1;
+    Console.WriteLine($"<p><b>Meddelande {messageNumber}:</b> {inMeddelande[messageNumber - 1]}.</p>");
+
+    if (messageNumber < inMeddelande.Length)
+    {
+        messageNumber++;
+        customMessage(inMeddelande, messageNumber);
+    }
+}
+
+csHTMLgenerator("C# Programmet", new string[] { "Vi börjar med Git", "Sedan fokuserar vi på Backend utveckling", "SQl Server", "Frontend" });
